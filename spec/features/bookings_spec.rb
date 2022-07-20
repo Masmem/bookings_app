364350385cd1b5c87771cd31cd8d02631dea7eb9
Matheus Leite <mlete@crainfra.com.br>
2022-07-20 15:40:31 -0300

    Adicionado nome aos usuários

--- spec/features/bookings_spec.rb
+++ spec/features/bookings_spec.rb
@@ -1,11 +1,30 @@
 require 'rails_helper'
+require 'faker'
 
 RSpec.describe BookingsHelper, type: :feature do
-    describe "Usuário não autenticado deve ser redirecionado para pagina de login" do
-        it "ao acessar o root" do
-          visit root_path
-          expect(page).to have_selector("div", class: "login-container")  
-        end
+  include Devise::Test::IntegrationHelpers
+
+  context "Usuário não autenticado" do
+    describe "deve ser redirecionado para pagina de login" do
+      it "ao acessar o root" do
+        visit root_path
+        expect(page).to have_selector("div", class: "login-container")  
+      end
     end
+  end
+
+  context "Usuário autenticado" do
+    before(:all) do
+      loged_used = User.create(email: Faker::Internet.email,  )
+    end
+    
+    describe "realizar logout no sistema" do
+      it "ser redirecionado para tela de login" do
+        
+      end
+      
+      
+    end
+  end
 end
   
