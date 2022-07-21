9633b358e278d2ed2167f518b54aa1bd213938be
Matheus Leite <mlete@crainfra.com.br>
2022-07-21 09:41:03 -0300

    Adicionado folha de estilos e testes para tela inicial

--- spec/features/bookings_spec.rb
+++ spec/features/bookings_spec.rb
@@ -16,16 +16,29 @@ RSpec.describe BookingsHelper, type: :feature do
   context "Usuário autenticado" do
     before(:all) do
       @loged_used = User.create(email: Faker::Internet.email, name: Faker::name, password: "teste" )
+    end
+
+    before(:each) do
       sign_in @loged_used
     end
     
     describe "realizar logout no sistema" do
       it "ser redirecionado para tela de login" do
         visit root_path
+        find(:css,"#email_dropdown_link").click
         find(:css, "#logout_link").click
-        expect(page).to have_selector("div", class: "login-container")
-      end     
+        expect(page).to have_selector("div", class: "login-container")        
+      end 
+    end
+
+    describe "acessar tela inicial" do
+      it "deve ixibir mensagem de boas-vindas" do
+        visit root_path
+        expect(page).to have_selector("div", id: "welcome-title")
+      end
       
+
+
     end
   end
 end
