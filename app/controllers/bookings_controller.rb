bb6dc035d898d74534039c0426f294ac3e834646
Matheus Leite <mlete@crainfra.com.br>
2022-07-22 11:30:16 -0300

    Adicionado turbo stream para destroy

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,12 +6,11 @@ class BookingsController < ApplicationController
     end
 
     def edit
-        p params
         @booking = current_user.bookings.find(params[:id])
     end
 
     def show
-        @booking = Booking.find(params[:id])   
+        @booking = Booking.find(params[:id])
     end
    
     def new
@@ -20,13 +19,12 @@ class BookingsController < ApplicationController
 
 
     def create
-        p params
         @booking = current_user.bookings.new(booking_params)
 
         respond_to do |format|
             if @booking.save!
                 @reservation_date = booking_params[:reservation_date].to_datetime
-                @past_hour = @reservation_date > Time.now
+                @past_hour = @reservation_date < Time.now
                 format.turbo_stream
             end
         end
@@ -38,26 +36,24 @@ class BookingsController < ApplicationController
        
        respond_to do |format|
             if @booking.update!(booking_params)
-                format.turbo_stream { render turbo_stream :index}
+                @reservation_date = booking_params[:reservation_date].to_datetime
+                @past_hour = @reservation_date < Time.now
+                format.turbo_stream
             end
-        end
-       
+        end       
     end
-
 
     def destroy
         @booking = current_user.bookings.find(params[:id])
-       
-        if @booking.delete
-            redirect_to bookings_url, status: :see_other
-        end
-    
+        @reservation_date = @booking.reservation_date   
+        respond_to do |format|
+            if @booking.delete
+                @booking = nil
+                @past_hour = @reservation_date < Time.now
+                format.turbo_stream
+            end
+        end    
     end
-    
-    
-    
-    
-
 
     private
 
