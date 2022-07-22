cdf23f9e374f48b34ebd6087dcc582f7caa5e3ac
Matheus Leite <mlete@crainfra.com.br>
2022-07-22 15:03:49 -0300

    Ajustado devise para logar sem turbo stream

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -67,8 +67,7 @@ class BookingsController < ApplicationController
         @week_reference = params.fetch(:week_reference, 0).to_i
         weekend_aux = @today.wday == 0 ? 1 : @today.wday == 6 ? 2 : 0 #variável auxiliar para ajustar a data quando sistema acessado em finais de semana
         @days = (1..5).to_a
-        p " TESTEE #{(7 * @week_reference)}"
-        @days = @days.map {|day| today = (@today + weekend_aux.days ); p (today.wday - day).days - (7 * @week_reference); today  - ((today.wday - day).days - (7 * @week_reference).days) }
+        @days = @days.map {|day| today = (@today + weekend_aux.days ); today  - ((today.wday - day).days - (7 * @week_reference).days) }
     end
 
     def booking_params
